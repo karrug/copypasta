30c94dcd1f6d7cccaf42b59c5cd9890e9fbc0fae
karrug <48385381+karrug@users.noreply.github.com>
2020-01-27 13:20:56 +0530

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -7,7 +7,7 @@ apt-get install \
     curl \
     gnupg2 \
     software-properties-common
-curl -fsSL https://download.docker.com/
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
