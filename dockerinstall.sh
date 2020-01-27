0828415c516731f41f647ee453a5d2f47d4a5ff6
karrug <48385381+karrug@users.noreply.github.com>
2020-01-27 13:17:58 +0530

    Create dockerinstall.sh

---
+++ dockerinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+apt-get update -y
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io
